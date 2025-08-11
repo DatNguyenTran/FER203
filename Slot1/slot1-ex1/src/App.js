b23f991a14baac3730e0b355448a60d4d35a9810
DatNguyenTran <dattnde180870@fpt.edu.vn>
2025-08-11 10:14:22 +0700

    Initial commit: Add FER202_Block3 structure

---
+++ Slot1/slot1-ex1/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+function App() {
+  const myName = "DatTran";
+
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>My name is {myName}</p>
+    </div>
+  );
+}
+
+export default App;
